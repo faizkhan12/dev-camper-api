d11a1c74ddbc18a6d831409b28cc05398a4eb6b2
faizkhan12 <faizk6797@gmail.com>
2021-01-28 20:35:14 +0530

    Connect with database and create bootcamp model

--- server.js
+++ server.js
@@ -1,13 +1,37 @@
 const express = require('express')
 const dotenv = require('dotenv')
+const morgan = require('morgan')
+const colors = require('colors')
+const connectDB = require('./config/db')
+const bootcamps = require('./routes/bootcamps')
 
 // load env vars
 dotenv.config({ path: './config/config.env' })
 
+// connect to database
+connectDB()
+
 const app = express()
+
+// dev logging middleware
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'))
+}
+
+// Mount routers
+app.use('/api/v1/bootcamps', bootcamps)
+
+
 
 const PORT = process.env.PORT || 8000
 
-app.listen(PORT, () => {
-    console.log(`Sever is up running at PORT ${process.env.PORT} `)
+const server = app.listen(PORT, () => {
+    console.log(`Server is up running at PORT ${process.env.PORT}`.green.bold)
+})
+
+// handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+    console.log(`Error: ${err.message}`.red)
+    // close server & exit process
+    server.close(() => process.exit(1))
 })
