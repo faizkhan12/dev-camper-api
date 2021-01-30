478dd22f913bb0ba9c1da2e1eef61103264ee517
faizkhan12 <faizk6797@gmail.com>
2021-01-30 19:07:30 +0530

    Add custom error handler

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,5 +1,5 @@
 const Bootcamp = require('../models/Bootcamp')
-
+const ErrorResponse = require('../utils/errorResponse')
 
 // @desc    GET all bootcamps
 // @route   GET /api/v1/bootcamps
@@ -10,14 +10,12 @@ exports.getBootcamps = async (req, res, next) => {
         const bootcamps = await Bootcamp.find()
         res.status(200).json({
             success: true,
+            count: bootcamps.length,
             data: bootcamps
         })
 
     } catch (err) {
-        res.status(400).json({
-            success: false
-        })
-
+        next(err)
     }
 }
 
@@ -29,9 +27,7 @@ exports.getBootcamp = async (req, res, next) => {
         const bootcamp = await Bootcamp.findById(req.params.id)
 
         if (!bootcamp) {
-            return res.status(400).json({
-                success: false
-            })
+            return next(new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404))
         }
         res.status(200).json({
             success: true,
@@ -39,9 +35,8 @@ exports.getBootcamp = async (req, res, next) => {
         })
 
     } catch (err) {
-        res.status(400).json({
-            success: false
-        })
+        next(err)
+
 
     }
 }
@@ -58,9 +53,7 @@ exports.createBootcamp = async (req, res, next) => {
             data: bootcamp
         })
     } catch (err) {
-        res.status(400).json({
-            success: false
-        })
+        next(err)
     }
 }
 
@@ -75,18 +68,15 @@ exports.updateBootcamp = async (req, res, next) => {
         })
 
         if (!bootcamp) {
-            return res.status(400).json({
-                success: false
-            })
+            return next(new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404))
+
         }
         res.status(200).json({
             success: true,
             data: bootcamp
         })
     } catch (err) {
-        res.status(400).json({
-            success: false
-        })
+        next(err)
     }
 }
 
@@ -98,18 +88,15 @@ exports.deleteBootcamp = async (req, res, next) => {
         const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id)
 
         if (!bootcamp) {
-            return res.status(400).json({
-                success: false
-            })
+            return next(new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404))
+
         }
         res.status(200).json({
             success: true,
             data: {}
         })
     } catch (err) {
-        res.status(400).json({
-            success: false
-        })
+        next(err)
     }
 }
 
--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const morgan = require('morgan')
 const colors = require('colors')
 const connectDB = require('./config/db')
 const bootcamps = require('./routes/bootcamps')
+const errorHandler = require('./middleware/error')
 
 // load env vars
 dotenv.config({ path: './config/config.env' })
@@ -23,6 +24,8 @@ if (process.env.NODE_ENV === 'development') {
 
 // Mount routers
 app.use('/api/v1/bootcamps', bootcamps)
+
+app.use(errorHandler)
 
 
 
