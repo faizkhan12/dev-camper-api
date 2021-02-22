b3c009a9b0c2535308338b2a1db44b344cb14b7c
faizkhan12 <faizk6797@gmail.com>
2021-02-22 21:48:15 +0530

    setup auth routes,controller and model

---
+++ models/User.js
@@ -0,0 +1,40 @@
+/*
+    models of user table 
+    author - @Faiz Khan
+*/
+const mongoose = require('mongoose')
+
+const UserSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Please add a name']
+    },
+    email: {
+        type: String,
+        required: [true, 'Please ass an email'],
+        match: [
+            /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+            'Please add a valid email'
+        ]
+    },
+    role: {
+        type: String,
+        enum: ['user', 'publisher'],
+        default: 'user'
+    },
+    password: {
+        type: String,
+        required: [true, 'Please add password'],
+        minlength: 6,
+        select: false,
+    },
+    resetPasswordToken: String,
+    resetPasswordExpire: Date,
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+
+})
+
+module.exports = mongoose.model('User', UserSchema)
--- server.js
+++ server.js
@@ -10,6 +10,7 @@ dotenv.config({ path: './config/config.env' })
 
 const bootcamps = require('./routes/bootcamps')
 const courses = require('./routes/courses')
+const auth = require('./routes/auth')
 const errorHandler = require('./middleware/error')
 const path = require('path')
 
@@ -35,6 +36,7 @@ app.use(express.static(path.join(__dirname, 'public')))
 // Mount routers
 app.use('/api/v1/bootcamps', bootcamps)
 app.use('/api/v1/courses', courses)
+app.use('/api/v1/auth', auth)
 
 app.use(errorHandler)
 
