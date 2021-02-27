b90f3d4365b4e38a303fa803bd5036bfe4b2ac49
faizkhan12 <faizk6797@gmail.com>
2021-02-27 19:12:33 +0530

    Add role authorization for each route

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -21,7 +21,7 @@ const courseRouter = require('./courses')
 
 const router = express.Router()
 
-const { protect } = require('../middleware/auth')
+const { protect, authorize } = require('../middleware/auth')
 
 // Re-route into other resource routers
 router.use('/:bootcampId/courses', courseRouter)
@@ -32,17 +32,17 @@ router
 
 router
     .route('/:id/photo')
-    .put(protect, uploadBootcampPhoto)
+    .put(protect, authorize('publisher', 'admin'), uploadBootcampPhoto)
 
 router
     .route('/')
     .get(advancedResults(Bootcamp, 'courses'), getBootcamps)
-    .post(protect, createBootcamp)
+    .post(protect, authorize('publisher', 'admin'), createBootcamp)
 
 router
     .route('/:id')
     .get(getBootcamp)
-    .put(protect, updateBootcamp)
-    .delete(protect, deleteBootcamp)
+    .put(protect, authorize('publisher', 'admin'), updateBootcamp)
+    .delete(protect, authorize('publisher', 'admin'), deleteBootcamp)
 
 module.exports = router
--- routes/courses.js
+++ routes/courses.js
@@ -16,7 +16,7 @@ const Course = require('../models/Course')
 
 const router = express.Router({ mergeParams: true })
 
-const { protect } = require('../middleware/auth')
+const { protect, authorize } = require('../middleware/auth')
 
 
 router
@@ -27,14 +27,14 @@ router
     }),
         getCourses
     )
-    .post(protect, createCourse)
+    .post(protect, authorize('publisher', 'admin'), createCourse)
 
 
 router
     .route('/:id')
     .get(getCourse)
-    .put(protect, updateCourse)
-    .delete(protect, deleteCourse)
+    .put(protect, authorize('publisher', 'admin'), updateCourse)
+    .delete(protect, authorize('publisher', 'admin'), deleteCourse)
 
 
 module.exports = router
