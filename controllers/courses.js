e1d83a297716739a522088506b983ad03f569c05
faizkhan12 <faizk6797@gmail.com>
2021-02-28 20:17:44 +0530

    grant only publisher related with course to update, delete

--- controllers/courses.js
+++ controllers/courses.js
@@ -7,6 +7,7 @@ const Course = require('../models/Course')
 const ErrorResponse = require('../utils/errorResponse')
 const asyncHandler = require('../middleware/async')
 const Bootcamp = require('../models/Bootcamp')
+const User = require('../models/User')
 
 // @desc    GET all courses
 // @route   GET /api/v1/courses
@@ -50,10 +51,17 @@ exports.getCourse = asyncHandler(async (req, res, next) => {
 // @access  Private
 exports.createCourse = asyncHandler(async (req, res, next) => {
     req.body.bootcamp = req.params.bootcampId
+    req.body.user = req.user.id
+
     const bootcamp = await Bootcamp.findById(req.params.bootcampId)
 
     if (!bootcamp) {
         return next(new ErrorResponse(`bootcamp not found with id of ${req.params.bootcampId}`, 404))
+    }
+
+    // make sure user is bootcamp owner 
+    if (bootcamp.user.toString() !== req.user.id && req.user.role !== 'admin') {
+        return next(new ErrorResponse(`User ${req.user.id} is not authorized to add a course to bootcamp ${bootcamp._id}`, 401))
     }
 
     // create new course
@@ -76,6 +84,12 @@ exports.updateCourse = asyncHandler(async (req, res, next) => {
         return next(new ErrorResponse(`Course not found with id of ${req.params.id}`, 404))
 
     }
+
+    // make sure user is bootcamp owner 
+    if (course.user.toString() !== req.user.id && req.user.role !== 'admin') {
+        return next(new ErrorResponse(`User ${req.user.id} is not authorized to update a course to bootcamp ${course._id}`, 401))
+    }
+
     course = await Course.findByIdAndUpdate(req.params.id, req.body, {
         new: true,
         runValidators: true
@@ -97,6 +111,11 @@ exports.deleteCourse = asyncHandler(async (req, res, next) => {
     if (!course) {
         return next(new ErrorResponse(`course not found with id of ${req.params.id}`, 404))
 
+    }
+
+    // make sure user is bootcamp owner 
+    if (course.user.toString() !== req.user.id && req.user.role !== 'admin') {
+        return next(new ErrorResponse(`User ${req.user.id} is not authorized to delete a course to bootcamp ${course._id}`, 401))
     }
     course.remove()
     res.status(200).json({
--- models/Course.js
+++ models/Course.js
@@ -40,6 +40,11 @@ const CourseSchema = new mongoose.Schema({
         type: mongoose.Schema.ObjectId,
         ref: 'Bootcamp',
         required: true
+    },
+    user: {
+        type: mongoose.Schema.ObjectId,
+        ref: 'User',
+        required: true
     }
 })
 // static method to get avg of course tuituions
